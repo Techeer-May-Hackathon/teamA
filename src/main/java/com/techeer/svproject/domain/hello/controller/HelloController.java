a498b76e8c7a14890f1b3ec3c148bcba229e2d8a
POL6463 <kevinkim0617kim@gmail.com>
2022-02-05 00:15:06 -0800

    build: add starer-web

---
+++ src/main/java/com/techeer/svproject/domain/hello/controller/HelloController.java
@@ -0,0 +1,9 @@
+package com.techeer.svproject.domain.hello.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class HelloController {
+    
+}
