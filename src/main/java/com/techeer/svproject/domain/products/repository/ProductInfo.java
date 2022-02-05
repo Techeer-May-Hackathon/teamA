d36c1b90ecb4d444de93fbb8dc226ef955093729
Somi <rlathal009@gmail.com>
2022-02-05 02:45:20 -0800

    feat: create product repository

--- src/main/java/com/techeer/svproject/domain/products/repository/ProductInfo.java
+++ src/main/java/com/techeer/svproject/domain/products/repository/ProductInfo.java
@@ -1,2 +1,10 @@
-package com.techeer.svproject.domain.products.repository;public interface ProductInfo {
+package com.techeer.svproject.domain.products.repository;
+
+import com.techeer.svproject.domain.products.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface ProductInfo extends JpaRepository<Product, UUID> {
 }
+
