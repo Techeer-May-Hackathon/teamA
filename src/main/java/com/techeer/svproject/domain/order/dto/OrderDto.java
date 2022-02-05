565c820516a04f5c7371263669fb136af20d0eed
Seohyeon_Lee <sheee7@naver.com>
2022-02-05 05:21:44 -0800

    feat: set order dto

---
+++ src/main/java/com/techeer/svproject/domain/order/dto/OrderDto.java
@@ -0,0 +1,30 @@
+package com.techeer.svproject.domain.order.dto;
+
+import com.techeer.svproject.domain.order.entity.Order;
+import com.techeer.svproject.domain.user.User;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderDto {
+    private UUID userId;
+    private LocalDateTime orderDate;
+
+    public Order toEntity() {
+        return Order.builder()
+                .orderDate(orderDate)
+                .build();
+    }
+
+    public static OrderDto fromEntity(Order order) {
+        return OrderDto.builder()
+                .userId(order.getUser().getId())
+                .orderDate(order.getOrderDate())
+                .build();
+    }
+}
