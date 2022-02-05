211e9cc2dc0ae73245f7abb35655365b2259c33b
POL6463 <kevinkim0617kim@gmail.com>
2022-02-05 05:20:47 -0800

    feat: add one to one relation

--- src/main/java/com/techeer/svproject/domain/user/User.java
+++ src/main/java/com/techeer/svproject/domain/user/User.java
@@ -1,19 +1,19 @@
 package com.techeer.svproject.domain.user;
 
+import com.techeer.svproject.domain.address.Address;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.hibernate.annotations.GenericGenerator;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.util.UUID;
 
 @Getter
 @NoArgsConstructor
 @Entity
+@Table(name = "account")
 public class User {
 
     @Id
@@ -36,13 +36,19 @@ public class User {
     @Column(nullable = false)
     private Integer phone_number;
 
+    @Setter
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "address_id", referencedColumnName = "address_id")
+    private Address address;
+
     @Builder
-    public User(String last_name, String first_name, String email, String password, Integer phone_number) {
+    public User(String last_name, String first_name, String email, String password, Integer phone_number, Address address) {
         this.last_name = last_name;
         this.first_name = first_name;
         this.email = email;
         this.password = password;
         this.phone_number = phone_number;
+        this.address = address;
     }
 
 }
