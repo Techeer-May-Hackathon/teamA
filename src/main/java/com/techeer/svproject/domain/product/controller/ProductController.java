f0c3183ba603ba599d259609e611123e07dc5123
Somi <rlathal009@gmail.com>
2022-02-06 10:18:42 -0800

    feat: add get product list by order id

--- src/main/java/com/techeer/svproject/domain/product/controller/ProductController.java
+++ src/main/java/com/techeer/svproject/domain/product/controller/ProductController.java
@@ -1,4 +1,5 @@
 package com.techeer.svproject.domain.product.controller;
+import com.techeer.svproject.domain.product.dto.ProductResponseDto;
 import com.techeer.svproject.domain.product.dto.ProductSaveDto;
 import com.techeer.svproject.domain.product.entity.Product;
 import com.techeer.svproject.domain.product.service.ProductService;
@@ -6,8 +7,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 import static com.techeer.svproject.global.utils.Constants.API_PREFIX;
 
@@ -16,6 +19,7 @@ import static com.techeer.svproject.global.utils.Constants.API_PREFIX;
 public class ProductController {
     private final ProductService productService;
 
+    /** 상품 저장 **/
     @ResponseBody
     @PostMapping(API_PREFIX + "/products")
     public ProductSaveDto save(@RequestBody ProductSaveDto productSaveDto) {
@@ -23,6 +27,7 @@ public class ProductController {
         return ProductSaveDto.fromEntity(product);
     }
 
+    /** 상품 조회 **/
     @ResponseBody
     @GetMapping(API_PREFIX+"/products/{id}")
     public ResponseEntity getDetail(@PathVariable UUID id) {
@@ -32,5 +37,18 @@ public class ProductController {
         return ResponseEntity
                 .ok()
                 .body(ProductSaveDto.fromEntity(entity.get()));
+    }
+
+    /** 주문 목록 조회 **/
+    @ResponseBody
+    @GetMapping(API_PREFIX+"/products/detail/{orderId}")
+    public ResponseEntity<List<ProductResponseDto>> getByOrderId(@PathVariable UUID orderId) {
+
+        List<ProductResponseDto> entity = productService.findAllByOrderId(orderId)
+                .stream()
+                .collect(Collectors.toList());
+        return ResponseEntity
+                .ok()
+                .body(productService.findAllByOrderId(orderId));
     }
 }
