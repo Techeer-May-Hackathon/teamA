18eec6a3d0ebf1fd995830537a7968528b9c96bf
Somi <rlathal009@gmail.com>
2022-02-05 16:02:45 -0800

    chore: add productsavedto from entity

--- src/main/java/com/techeer/svproject/domain/products/dto/ProductSaveDto.java
+++ src/main/java/com/techeer/svproject/domain/products/dto/ProductSaveDto.java
@@ -1,6 +1,7 @@
 package com.techeer.svproject.domain.products.dto;
 
 import com.techeer.svproject.domain.products.entity.Product;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,24 +9,26 @@ import lombok.NoArgsConstructor;
 import java.util.UUID;
 
 @Getter
+@Builder
+@AllArgsConstructor
 @NoArgsConstructor
 public class ProductSaveDto {
     private UUID orderId;
     private int price;
     private String productName;
 
-    @Builder
-    public ProductSaveDto(UUID orderId, int price, String productName) {
-        this.orderId = orderId;
-        this.price = price;
-        this.productName = productName;
-    }
-
     public Product toEntity() {
         return Product.builder()
-                .orderId(orderId)
                 .price(price)
                 .productName(productName)
+                .build();
+    }
+
+    public static ProductSaveDto fromEntity(Product product) {
+        return ProductSaveDto.builder()
+                .orderId(product.getOrder().getId())
+                .price(product.getPrice())
+                .productName(product.getProductName())
                 .build();
     }
 }
