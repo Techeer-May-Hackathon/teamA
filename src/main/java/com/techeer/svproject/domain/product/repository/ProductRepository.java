f00b3b6d2845dda59d75175bf1f9bb9abee33fb4
Somi <rlathal009@gmail.com>
2022-02-06 10:19:12 -0800

    feat: add findByOrderId

--- src/main/java/com/techeer/svproject/domain/product/repository/ProductRepository.java
+++ src/main/java/com/techeer/svproject/domain/product/repository/ProductRepository.java
@@ -3,7 +3,9 @@ package com.techeer.svproject.domain.product.repository;
 import com.techeer.svproject.domain.product.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface ProductRepository extends JpaRepository<Product, UUID> {
+    List<Product> findByOrderId(UUID id);
 }
--- src/main/java/com/techeer/svproject/domain/product/service/ProductService.java
+++ src/main/java/com/techeer/svproject/domain/product/service/ProductService.java
@@ -7,6 +7,7 @@ import com.techeer.svproject.domain.product.dto.ProductSaveDto;
 import com.techeer.svproject.domain.product.entity.Product;
 import com.techeer.svproject.domain.product.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Example;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -37,5 +38,11 @@ public class ProductService {
     @Transactional(readOnly = true)
     public Optional<Product> findById(UUID id){
         return productRepository.findById(id);
+    }
+
+    @Transactional(readOnly = true)
+    public List<ProductResponseDto> findAllByOrderId(UUID id) {
+//        return productRepository.findByOrderId(id);
+        return productRepository.findByOrderId(id).stream().map(ProductResponseDto::new).collect(Collectors.toList());
     }
 }
