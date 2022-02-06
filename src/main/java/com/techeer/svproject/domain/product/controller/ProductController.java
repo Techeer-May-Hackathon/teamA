bc7ab563017e778adf65caeda072c2b205563db7
Somi <rlathal009@gmail.com>
2022-02-06 15:30:10 -0800

    feat: add deleteProduct api

--- src/main/java/com/techeer/svproject/domain/product/controller/ProductController.java
+++ src/main/java/com/techeer/svproject/domain/product/controller/ProductController.java
@@ -3,7 +3,10 @@ import com.techeer.svproject.domain.product.dto.ProductResponseDto;
 import com.techeer.svproject.domain.product.dto.ProductSaveDto;
 import com.techeer.svproject.domain.product.entity.Product;
 import com.techeer.svproject.domain.product.service.ProductService;
+import com.techeer.svproject.global.utils.dto.ErrorResponseDto;
 import lombok.RequiredArgsConstructor;
+import org.apache.coyote.Response;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,12 +19,13 @@ import static com.techeer.svproject.global.utils.Constants.API_PREFIX;
 
 @RequiredArgsConstructor
 @RestController
+@RequestMapping(API_PREFIX+"/products")
 public class ProductController {
     private final ProductService productService;
 
     /** 상품 저장 **/
     @ResponseBody
-    @PostMapping(API_PREFIX + "/products")
+    @PostMapping
     public ProductSaveDto save(@RequestBody ProductSaveDto productSaveDto) {
         Product product = productService.save(productSaveDto);
         return ProductSaveDto.fromEntity(product);
@@ -29,11 +33,10 @@ public class ProductController {
 
     /** 상품 조회 **/
     @ResponseBody
-    @GetMapping(API_PREFIX+"/products/{id}")
+    @GetMapping("/{id}")
     public ResponseEntity getDetail(@PathVariable UUID id) {
 
         Optional<Product> entity = productService.findById(id);
-
         return ResponseEntity
                 .ok()
                 .body(ProductSaveDto.fromEntity(entity.get()));
@@ -41,14 +44,30 @@ public class ProductController {
 
     /** 주문 목록 조회 **/
     @ResponseBody
-    @GetMapping(API_PREFIX+"/products/detail/{orderId}")
-    public ResponseEntity<List<ProductResponseDto>> getByOrderId(@PathVariable UUID orderId) {
+    @GetMapping
+    public ResponseEntity<List<ProductResponseDto>> getByOrderId(@RequestParam UUID orderId) {
 
         List<ProductResponseDto> entity = productService.findAllByOrderId(orderId)
                 .stream()
                 .collect(Collectors.toList());
+        try{
+            return ResponseEntity
+                    .ok()
+                    .body(productService.findAllByOrderId(orderId));
+        }
+        catch(Exception e) {
+            return new ResponseEntity(ErrorResponseDto.fromEntity("FORBIDDEN", "상품 목록 조회에 오류가 발생하였습니다."), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    /** 상품 삭제 **/
+    @ResponseBody
+    @DeleteMapping("/{id}")
+    public ResponseEntity<ProductResponseDto> deleteProduct(@PathVariable UUID id){
+
+        productService.delete(id);
         return ResponseEntity
                 .ok()
-                .body(productService.findAllByOrderId(orderId));
+                .build();
     }
 }
