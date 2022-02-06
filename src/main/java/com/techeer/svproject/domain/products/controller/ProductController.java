78ecc98cc7d3ee5f42aa0c7ea8d3fe6fbc0bb7fb
Somi <rlathal009@gmail.com>
2022-02-05 16:02:57 -0800

    chore: return productsavedto

--- src/main/java/com/techeer/svproject/domain/products/controller/ProductController.java
+++ src/main/java/com/techeer/svproject/domain/products/controller/ProductController.java
@@ -1,9 +1,11 @@
 package com.techeer.svproject.domain.products.controller;
 import com.techeer.svproject.domain.products.dto.ProductSaveDto;
+import com.techeer.svproject.domain.products.entity.Product;
 import com.techeer.svproject.domain.products.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.UUID;
@@ -15,8 +17,10 @@ import static com.techeer.svproject.global.utils.Constants.API_PREFIX;
 public class ProductController {
     private final ProductService productService;
 
+    @ResponseBody
     @PostMapping(API_PREFIX + "/products")
-    public UUID save(@RequestBody ProductSaveDto requestDTO) {
-        return productService.save(requestDTO);
+    public ProductSaveDto save(@RequestBody ProductSaveDto productSaveDto) {
+        Product product = productService.save(productSaveDto);
+        return ProductSaveDto.fromEntity(product);
     }
 }
